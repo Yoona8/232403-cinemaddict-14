bd84748f268fc3984dd8213703f221d68c16b3ff
Yoona8 <elena243v@gmail.com>
2021-04-02 12:05:09 +0300

    [2] Add webpack-dev-server configurations

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,10 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 4200,
+    watchContentBase: true
+  }
 };
