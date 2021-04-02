d173f9f840e95a0e923540a512a967cfd1179fba
Yoona8 <elena243v@gmail.com>
2021-04-02 17:56:38 +0300

    [3] Add user view

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import {getUserTemplate} from './views/user-view';
+
+const RenderPosition = {
+  BEFORE_END: 'beforeend',
+};
+
+const render = (
+  container,
+  template,
+  position = RenderPosition.BEFORE_END
+) => {
+  container.insertAdjacentHTML(position, template);
+};
+
+const headerElement = document.querySelector('.header');
+
+render(headerElement, getUserTemplate());
