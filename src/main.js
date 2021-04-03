608e16bea487ff5a4019dfbbe1e572f52a53c631
Yoona8 <elena243v@gmail.com>
2021-04-03 10:35:35 +0300

    [3] Add movie template

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import {getUserTemplate} from './views/user-view';
 import {getMenuTemplate} from './views/menu-view';
 import {getSortingTemplate} from './views/sorting-view';
 import {getMoviesTemplate} from './views/movies-view';
+import {getMovieTemplate} from './views/movie-view';
 
 const RenderPosition = {
   BEFORE_END: 'beforeend',
 };
+
+const MoviesCount = {
+  ALL: 5,
+  TOP_RATED: 2,
+  COMMENTED: 2,
+};
+
+const movies = new Array(MoviesCount.ALL).fill('');
 
 const render = (
   container,
@@ -24,3 +33,17 @@ const mainElement = document.querySelector('.main');
 render(mainElement, getMenuTemplate());
 render(mainElement, getSortingTemplate());
 render(mainElement, getMoviesTemplate());
+
+const moviesElement = mainElement.querySelector('[data-movies]');
+
+movies.forEach(() => render(moviesElement, getMovieTemplate()));
+
+const topRatedElement = mainElement.querySelector('[data-top-rated]');
+
+movies.slice(0, MoviesCount.COMMENTED)
+  .forEach(() => render(topRatedElement, getMovieTemplate()));
+
+const mostCommentedElement = mainElement.querySelector('[data-commented]');
+
+movies.slice(0, MoviesCount.COMMENTED)
+  .forEach(() => render(mostCommentedElement, getMovieTemplate()));
--- src/views/movies-view.js
+++ src/views/movies-view.js
@@ -5,21 +5,21 @@ export const getMoviesTemplate = () => {
         <h2 class="films-list__title visually-hidden">
           All movies. Upcoming
         </h2>
-        <div class="films-list__container"></div>
+        <div class="films-list__container" data-movies></div>
       </section>
 
       <section class="films-list films-list--extra">
         <h2 class="films-list__title">
           Top rated
         </h2>
-        <div class="films-list__container"></div>
+        <div class="films-list__container" data-top-rated></div>
       </section>
 
       <section class="films-list films-list--extra">
         <h2 class="films-list__title">
           Most commented
         </h2>
-        <div class="films-list__container"></div>
+        <div class="films-list__container" data-commented></div>
       </section>
     </section>
   `.trim();
