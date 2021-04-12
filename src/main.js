622751d806134771adb88b62cb5cc0d241fda76f
Yoona8 <elena243v@gmail.com>
2021-04-12 09:16:45 +0300

    [6] Add NoMovies class

--- src/main.js
+++ src/main.js
@@ -5,12 +5,13 @@ import MoviesView from './views/movies';
 import MovieView from './views/movie';
 import ShowMoreButtonView from './views/show-more-button';
 import MoviesTotalView from './views/movies-total';
+import DetailsModalView from './views/details-modal';
+import NoMoviesView from './views/no-movies';
 import {getMovies} from './mocks/movies';
 import {getUser} from './mocks/user';
 import {getComments} from './mocks/comments';
 import {getFilters} from './mocks/filters';
 import {render, RenderPosition} from './helpers/render';
-import DetailsModal from './views/details-modal';
 import {checkEscKeyDown} from './helpers/helpers';
 
 const MoviesCount = {
@@ -36,92 +37,101 @@ const mainElement = document.querySelector('.main');
 
 render(mainElement, new MenuView(filters).getElement());
 render(mainElement, new SortingView().getElement());
-render(mainElement, new MoviesView().getElement());
 
-const openDetails = (movie) => {
-  const detailsModalView = new DetailsModal(movie, user, comments);
+const renderMovies = () => {
+  render(mainElement, new MoviesView().getElement());
 
-  const closeDetails = () => {
-    document.body.classList.remove(BODY_NO_SCROLL_CLASS_NAME);
-    document.removeEventListener('keydown', onDetailsEscKeyDown);
-    detailsModalView.removeElement();
-  };
+  const openDetails = (movie) => {
+    const detailsModalView = new DetailsModalView(movie, user, comments);
 
-  const onDetailsClose = () => {
-    closeDetails();
-  };
+    const closeDetails = () => {
+      document.body.classList.remove(BODY_NO_SCROLL_CLASS_NAME);
+      document.removeEventListener('keydown', onDetailsEscKeyDown);
+      detailsModalView.removeElement();
+    };
 
-  const onDetailsEscKeyDown = (evt) => {
-    if (checkEscKeyDown(evt.key)) {
+    const onDetailsClose = () => {
       closeDetails();
-    }
+    };
+
+    const onDetailsEscKeyDown = (evt) => {
+      if (checkEscKeyDown(evt.key)) {
+        closeDetails();
+      }
+    };
+
+    detailsModalView.addCloseListener(onDetailsClose);
+    document.addEventListener('keydown', onDetailsEscKeyDown);
+    document.body.classList.add(BODY_NO_SCROLL_CLASS_NAME);
+    render(document.body, detailsModalView.getElement());
   };
 
-  detailsModalView.addCloseListener(onDetailsClose);
-  document.addEventListener('keydown', onDetailsEscKeyDown);
-  document.body.classList.add(BODY_NO_SCROLL_CLASS_NAME);
-  render(document.body, detailsModalView.getElement());
+  const onDetailsOpen = (movie) => {
+    openDetails(movie);
+  };
+
+  const renderMovie = (container, movie) => {
+    const movieView = new MovieView(movie, user);
+
+    movieView.addOpenDetailsListener(() => onDetailsOpen(movie));
+    render(container, movieView.getElement());
+  };
+
+  const moviesElement = mainElement.querySelector('[data-movies]');
+
+  movies.slice(0, MoviesCount.PER_STEP)
+    .forEach((movie) => renderMovie(moviesElement, movie));
+
+  const topRatedElement = mainElement.querySelector('[data-top-rated]');
+
+  movies.slice()
+    .sort((a, b) => b.rating - a.rating)
+    .slice(0, MoviesCount.TOP_RATED)
+    .forEach((movie) => renderMovie(topRatedElement, movie));
+
+  const mostCommentedElement = mainElement.querySelector('[data-commented]');
+
+  movies.slice()
+    .sort((a, b) => b.comments.length - a.comments.length)
+    .slice(0, MoviesCount.COMMENTED)
+    .forEach((movie) => renderMovie(mostCommentedElement, movie));
+
+  if (movies.length > MoviesCount.PER_STEP) {
+    let renderedMoviesCount = MoviesCount.PER_STEP;
+
+    render(
+      moviesElement,
+      new ShowMoreButtonView().getElement(),
+      RenderPosition.AFTER_END,
+    );
+
+    const loadMoreButtonElement = mainElement
+      .querySelector('.films-list__show-more');
+
+    const onLoadMoreButtonClick = (evt) => {
+      evt.preventDefault();
+
+      movies
+        .slice(renderedMoviesCount, renderedMoviesCount + MoviesCount.PER_STEP)
+        .forEach((movie) => renderMovie(moviesElement, movie));
+
+      renderedMoviesCount += MoviesCount.PER_STEP;
+
+      if (renderedMoviesCount >= movies.length) {
+        loadMoreButtonElement.remove();
+      }
+    };
+
+    loadMoreButtonElement.addEventListener('click', onLoadMoreButtonClick);
+  }
 };
 
-const onDetailsOpen = (movie) => {
-  openDetails(movie);
-};
-
-const renderMovie = (container, movie) => {
-  const movieView = new MovieView(movie, user);
-
-  movieView.addOpenDetailsListener(() => onDetailsOpen(movie));
-  render(container, movieView.getElement());
-};
-
-const moviesElement = mainElement.querySelector('[data-movies]');
-
-movies.slice(0, MoviesCount.PER_STEP)
-  .forEach((movie) => renderMovie(moviesElement, movie));
-
-const topRatedElement = mainElement.querySelector('[data-top-rated]');
-
-movies.slice()
-  .sort((a, b) => b.rating - a.rating)
-  .slice(0, MoviesCount.TOP_RATED)
-  .forEach((movie) => renderMovie(topRatedElement, movie));
-
-const mostCommentedElement = mainElement.querySelector('[data-commented]');
-
-movies.slice()
-  .sort((a, b) => b.comments.length - a.comments.length)
-  .slice(0, MoviesCount.COMMENTED)
-  .forEach((movie) => renderMovie(mostCommentedElement, movie));
+if (movies.length === 0) {
+  render(mainElement, new NoMoviesView().getElement());
+} else {
+  renderMovies();
+}
 
 const moviesTotalElement = document.querySelector('.footer__statistics');
 
 render(moviesTotalElement, new MoviesTotalView(movies.length).getElement());
-
-if (movies.length > MoviesCount.PER_STEP) {
-  let renderedMoviesCount = MoviesCount.PER_STEP;
-
-  render(
-    moviesElement,
-    new ShowMoreButtonView().getElement(),
-    RenderPosition.AFTER_END,
-  );
-
-  const loadMoreButtonElement = mainElement
-    .querySelector('.films-list__show-more');
-
-  const onLoadMoreButtonClick = (evt) => {
-    evt.preventDefault();
-
-    movies
-      .slice(renderedMoviesCount, renderedMoviesCount + MoviesCount.PER_STEP)
-      .forEach((movie) => renderMovie(moviesElement, movie));
-
-    renderedMoviesCount += MoviesCount.PER_STEP;
-
-    if (renderedMoviesCount >= movies.length) {
-      loadMoreButtonElement.remove();
-    }
-  };
-
-  loadMoreButtonElement.addEventListener('click', onLoadMoreButtonClick);
-}
