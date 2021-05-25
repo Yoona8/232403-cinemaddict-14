d3533c2e97293297294e8ba344baaf6db4b56673
Yoona8 <elena243v@gmail.com>
2021-05-25 08:28:39 +0300

    [9.5] Add store

---
+++ src/api/store.js
@@ -0,0 +1,18 @@
+export default class Store {
+  constructor(key, storage) {
+    this._key = key;
+    this._storage = storage;
+  }
+
+  getItems() {
+    try {
+      return JSON.parse(this._storage.getItem(this._key)) || {};
+    } catch (error) {
+      return {};
+    }
+  }
+
+  setItems(items) {
+    this._storage.setItem(this._key, JSON.stringify(items));
+  }
+}
